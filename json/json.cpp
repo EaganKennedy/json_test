bb4cc516a8547a878aa4665f8932acc09e260dda
Eagan <eegykennedy@gmail.com>
2025-10-29 20:27:13 -0500

    Add project files.

---
+++ json/json.cpp
@@ -0,0 +1,5 @@
+#include "json.hpp"
+
+namespace json {
+	IValue::~IValue() = default;
+}
---
+++ json/json.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+
+namespace json {
+	class IValue;
+	using Value = std::shared_ptr<IValue>;
+	using ValueVector = std::vector<Value>;
+	using ValueMap = std::map<std::string, Value>;
+
+	class IValue {
+	public:
+		IValue() = delete;
+
+		virtual ~IValue() = 0;
+	};
+}
