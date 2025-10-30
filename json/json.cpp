875c5d2f80a7f2f044e44f31098b879eb521877c
Eagan <eegykennedy@gmail.com>
2025-10-29 20:46:52 -0500

    json Base Class implementation complted

--- json/json.cpp
+++ json/json.cpp
@@ -2,4 +2,39 @@
 
 namespace json {
 	IValue::~IValue() = default;
+
+	bool IValue::isNull(){
+		return false;
+	}
+	bool IValue::isBoolean(){
+		return false;
+	}
+	bool IValue::isNumber(){
+		return false;
+	}
+	bool IValue::isString(){
+		return false;
+	}
+	bool IValue::isArray(){
+		return false;
+	}
+	bool IValue::isObject(){
+		return false;
+	}
+
+	bool& IValue::getBool(){
+		throw TypeError("This object is not a boolean");
+	}
+	double& IValue::getNumber(){
+		throw TypeError("This object is not a number");
+	}
+	std::string IValue::getString(){
+		throw TypeError("This object is not a string");
+	}
+	ValueVector& IValue::getArray(){
+		throw TypeError("This object is not an array");
+	}
+	ValueMap& IValue::getObject(){
+		throw TypeError("This object is not an Object");
+	}
 }
