c85cdccda78a09c86365850bacec7d0d01f05ff4
Eagan <eegykennedy@gmail.com>
2025-10-31 13:51:46 -0500

    Header for Array made

--- json/array.hpp
+++ json/array.hpp
@@ -1,5 +1,29 @@
-#include "ivalue.hpp"
+#include "iData.hpp"
+#include <vector>
 
 namespace json {
+	class Array : public IData {
+	public:
+		Array(DataVector dv);
 
+		void print(std::ostream& out) const override;
+		Data clone() const override;
+
+		bool isArray() const override;
+		DataVector& getArray() override;
+
+	protected:
+		DataVector data;
+
+	};
+
+	Data toData(DataVector dv);
+	template<typename T>
+	Data toData(std::vector<T> v) {
+		
+	}
+	template<typename T>
+	std::vector<T> fromJson(DataVector dv, T t) {
+
+	}
 }
